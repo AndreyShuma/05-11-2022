ca29c7d6144626919162081555cbecd90222c5eb
AndreyShuma <0662389612a@gmail.com>
2022-11-11 13:41:52 +0200

    Carpachov

--- JS/JS.js
+++ JS/JS.js
@@ -23,3 +23,32 @@ myCity.adress[userName]="undefind"
 myCity[fineCity] = "Yes";
 console.log(myCity);
 myCity.cityGreeting();
+let k;
+let l;
+function mum(k,l) {
+    const n = k+l;
+    console.log(n)
+};
+// mum(10, 30);
+// console.dir(mum);
+// console.dir(mum.name);
+// console.dir(mum.length);
+const personOne = {
+    name: "Bob",
+    age: 21
+}
+const personOne1111 = {
+    name: "Bob"
+
+}
+
+function increasePersonAge (fhjhjhjk) {
+    fhjhjhjk.age +=1;
+    return fhjhjhjk
+}
+
+increasePersonAge(personOne);
+increasePersonAge(personOne1111);
+console.log(personOne);
+console.log(personOne1111);
+setTimeout()
